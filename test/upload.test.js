e16ac555b78f2628cd6a33fd4569d04cd11432df
Richard Girges <richardgirges@gmail.com>
2017-01-13 15:33:45 -0800

    more conventional indentations

--- test/upload.test.js
+++ test/upload.test.js
@@ -1,6 +1,6 @@
 var express    = require('express'),
-	fileUpload = require('../lib/index.js'),
-	app        = express();
+  fileUpload = require('../lib/index.js'),
+  app        = express();
 
 app.use('/form', express.static(__dirname + '/upload.test.html'));
 
@@ -8,31 +8,31 @@ app.use('/form', express.static(__dirname + '/upload.test.html'));
 app.use(fileUpload());
 
 app.get('/ping', function(req, res) {
-	res.send('pong');
+  res.send('pong');
 });
 
 app.post('/upload', function(req, res) {
-	var sampleFile, uploadPath;
+  var sampleFile, uploadPath;
 
-	if (!req.files) {
-		res.status(400).send('No files were uploaded.');
-		return;
-	}
+  if (!req.files) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
 
-	sampleFile = req.files.sampleFile;
+  sampleFile = req.files.sampleFile;
 
-	uploadPath = __dirname + '/uploadedfiles/' + sampleFile.name;
+  uploadPath = __dirname + '/uploadedfiles/' + sampleFile.name;
 
-	sampleFile.mv(uploadPath, function(err) {
-		if (err) {
-			res.status(500).send(err);
-		}
-		else {
-			res.send('File uploaded to ' + uploadPath);
-		}
-	});
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+    else {
+      res.send('File uploaded to ' + uploadPath);
+    }
+  });
 });
 
 app.listen(8000, function() {
-	console.log('Express server listening on port 8000');
+  console.log('Express server listening on port 8000');
 })
