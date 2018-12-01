e5b77f10e96e963bee4612947f8b952d408498dc
quilty <quiltykim1@gmail.com>
2018-12-01 12:00:08 -0800

    add dried out version of tempFileHandler as option for saving files with test. passes all tests

--- example/server.js
+++ example/server.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const fileUpload = require('../lib/index.js');
+const express = require("express");
+const fileUpload = require("express-fileupload");
 const app = express();
 
-app.use('/form', express.static(__dirname + '/index.html'));
+app.use("/form", express.static(__dirname + "/index.html"));
 
 // default options
 app.use(fileUpload());
 
-app.get('/ping', function(req, res) {
-  res.send('pong');
+app.get("/ping", function(req, res) {
+  res.send("pong");
 });
 
-app.post('/upload', function(req, res) {
+app.post("/upload", function(req, res) {
   let sampleFile;
   let uploadPath;
 
   if (Object.keys(req.files).length == 0) {
-    res.status(400).send('No files were uploaded.');
+    res.status(400).send("No files were uploaded.");
     return;
   }
 
-  console.log('req.files >>>', req.files); // eslint-disable-line
+  console.log("req.files >>>", req.files); // eslint-disable-line
 
   sampleFile = req.files.sampleFile;
 
-  uploadPath = __dirname + '/uploads/' + sampleFile.name;
+  uploadPath = __dirname + "/uploads/" + sampleFile.name;
 
   sampleFile.mv(uploadPath, function(err) {
     if (err) {
       return res.status(500).send(err);
     }
 
-    res.send('File uploaded to ' + uploadPath);
+    res.send("File uploaded to " + uploadPath);
   });
 });
 
-app.listen(8000, function() {
-  console.log('Express server listening on port 8000'); // eslint-disable-line
+app.listen(8002, function() {
+  console.log("Express server listening on port 8002"); // eslint-disable-line
 });
