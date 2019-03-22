494e378de794fdf380048a5ef4cd6403ccf9ac14
Roman Burunkov <romon2002@gmail.com>
2019-03-22 14:45:28 +0300

    Make description shorter

--- test/utilities.spec.js
+++ test/utilities.spec.js
@@ -122,7 +122,7 @@ describe('Test of the utilities functions', function() {
       });
 
     it(
-      'Strips away all non-alphanumeric chars when enabled for a name which doesnt contain dots characters and preserveExtension: true.',
+      'Strips away all non-alphanumeric chars when enabled and preserveExtension: true for a name without dot characters',
       () => {
         const opts = {safeFileNames: true, preserveExtension: true};
         const name = 'my$Invalid#fileName';
